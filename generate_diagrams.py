0826ade1fc53dda93df6371e589f341ba504ae43
guillesanbri <guillesanbri@hotmail.com>
2021-08-29 13:34:36 +0200

    Minor fixes on generate_diagrams.py

--- generate_diagrams.py
+++ generate_diagrams.py
@@ -9,12 +9,7 @@ if __name__ == "__main__":
     # Read texts
 
     # Dictionary of id:element_suffix
-    # Element tagging can be overwritten as follows
-    # There is no point in doing this here, annotations will be transformed
-    # before training to ensure consistency between train/val/test
-    # shapes_dict = {'400': 'shape', '600': 'shape', '800': 'shape'}
     ids_suffixes = {**shapes_dict, **connections_dict}
 
     dg = DiagramGenerator("diagrams/", 10, (5, 15), seed=42, debug=True)
     dg.run(shapes_paths, connections_paths, None, ids_suffixes)
-    # dg.save_annotation_as(annotations, "VOC")  # Convert to typical bbox annotation formats.
